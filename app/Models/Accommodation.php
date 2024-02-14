d184523d6d1ea81ae4db35a2151f4bd1a90384ec
dwaichari <dwaichari@gmail.com>
2024-02-14 08:35:18 +0300

    design schema for accommodation model

--- app/Models/Accommodation.php
+++ app/Models/Accommodation.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Accommodation extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'standard_rack_rate',
+    ];
 }
